3f1a6635f82571cec118baad5533fc8fa6dbf66d
KevinIturbe <iturbegerardo00@gmail.com>
2022-05-07 18:39:31 -0500

    He iniciado el primer Challenge, tomado nota de los demás cursos

---
+++ Primer Challenge/encriptacion.js
@@ -0,0 +1,10 @@
+function encriptar (texto)
+{
+    if (texto == "e" ){document.write("enter")}
+    else if (i) {imes}
+    else if (a) {ai}
+    else if (o) {ober}
+    else if (u) {ufat}
+}
+
+encriptar("enemigo")
